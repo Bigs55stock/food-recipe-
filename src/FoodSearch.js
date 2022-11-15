c4efe5b60bbc00480734ba070c4a6c72d6740575
humza15 <humzazafarullah@gmail.com>
2022-11-15 12:30:43 -0500

    search bar

--- src/FoodSearch.js
+++ src/FoodSearch.js
@@ -9,6 +9,7 @@ function FoodSearch (){
     const [url, setUrl] =useState('https:/www.themealdb.com/api/json/v1/1/search.php?f=a')
     const [object,setObject] =useState()
     const [view,setView] =useState(false)
+    const [search, setSearch]= useState("")
     
     useEffect(() => {
         fetch(url)
@@ -23,6 +24,12 @@ function FoodSearch (){
     const setIndex=(letter) =>{
         setUrl(`https:/www.themealdb.com/api/json/v1/1/search.php?f=${letter}`)
     }
+
+    const searchFood=(evt)=> {
+        if (evt.key == "Enter"){
+            setUrl(`https:/www.themealdb.com/api/json/v1/1/search.php?s=${search}`)
+        }
+    }
     
     return (
         <div className="foodserContainer">
@@ -31,7 +38,7 @@ function FoodSearch (){
                 <h4>hello you get all types of food here</h4>
             </div>
             <div className="search">
-                <input type="search" className="search-bar" />
+                <input type="search" className="search-bar" onChange={e=>setSearch(e.target.value)} onKeyPress={searchFood}/>
             </div>
             <div className="Cardcontainer">
               
