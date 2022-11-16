687166bfc1a3a75930d6e9b4342c88573c601d64
humza15 <humzazafarullah@gmail.com>
2022-11-16 11:29:02 -0500

    homepage base css

--- src/Home.js
+++ src/Home.js
@@ -6,8 +6,8 @@ const Home = () => {
     return (
       <div className="Homecontainer">
         <h1 className="Home-h1">Welcome To Easy Foods </h1>
-        <p className="Home-p">Here we have all types of different Food recipes which gives you a step by step instructions to cook</p>
-        <Link to="/foods">Food search</Link>
+        <p className="Home-p">Here we have all types of different Food recipes with a step by step instruction to make the dish. Click on the food icon to get started. </p>
+        <Link className="iconfood" to="/foods">🥘</Link>
       </div>
     );
 }
