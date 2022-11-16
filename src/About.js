9684ff544f6960562588607cc0e3ebb2318dc9da
humza15 <humzazafarullah@gmail.com>
2022-11-16 12:06:09 -0500

    about base css

--- src/About.js
+++ src/About.js
@@ -3,11 +3,11 @@ import React from "react";
 
 const About = () => {
     return (
-      <div>
-        <h1>This is the About!</h1>
-        <p>This page shows you how to navigate on this page</p>
-        <p>Click on Home to go home</p>
-        <p> Click on Food icon to see and search food recipes. Click on foods to get food recipe</p>
+      <div className="Aboutcontainer">
+        <h1 className="Aboutheading">This is the About!</h1>
+        <p className="Home-p">This page shows you how to navigate on this page</p>
+        <p className="Home-p">Click on Home icon to go home</p>
+        <p className="Home-p"> Click on Food icon to see and search food recipes. Click on foods to get food recipe</p>
       </div>
     );
 }
