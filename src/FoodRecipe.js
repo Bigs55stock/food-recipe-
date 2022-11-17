fefce515adbab30f306a43e4d052019f244442ff
humza15 <humzazafarullah@gmail.com>
2022-11-17 16:32:54 -0500

    envion variables

--- src/FoodRecipe.js
+++ src/FoodRecipe.js
@@ -7,7 +7,7 @@ function FoodRecipe() {
     const{MealId}=useParams()
     if(MealId!==""){
         
-        fetch(`https:/www.themealdb.com/api/json/v2/9973533/lookup.php?i=${MealId}`)
+        fetch(`https:/www.themealdb.com/api/json/API_URL/lookup.php?i=${MealId}`)
             .then(res=> res.json())
             .then(data => {
                 setObject(data.meals[0]);
