502df829dbb7c501ad0d4d17288c3aa56aa7b635
humza15 <humzazafarullah@gmail.com>
2022-11-17 18:37:56 -0500

    APIKEYS2

--- src/FoodRecipe.js
+++ src/FoodRecipe.js
@@ -7,7 +7,7 @@ function FoodRecipe() {
     const{MealId}=useParams()
     if(MealId!==""){
         
-        fetch(`https:/www.themealdb.com/api/json/v2/${process.env.APIKEYS}/lookup.php?i=${MealId}`, {
+        fetch(`https:/www.themealdb.com/api/json/v2/${process.env.REACT_APP_APIKEYS}/lookup.php?i=${MealId}`, {
             headers: {
                 'Content-Type': 'application/json'
               },
